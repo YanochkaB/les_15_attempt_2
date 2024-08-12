963e302b7f205319eb5c52e3127d203ccacb5f93
YanochkaB <50401599+YanochkaB@users.noreply.github.com>
2024-08-12 20:48:36 +0000

    Deploying to gh-pages from @ YanochkaB/les_15_attempt_2@6dc585af2690f31488b8121712f82b4a780ab269 🚀

---
+++ index.07b14da3.js
@@ -0,0 +1,2 @@
+!function(){console.log("hi");console.log("a+b",20)}();
+//# sourceMappingURL=index.07b14da3.js.map
---
+++ index.1906beee.js
@@ -0,0 +1,2 @@
+console.log("hi");console.log("a+b",20);
+//# sourceMappingURL=index.1906beee.js.map
